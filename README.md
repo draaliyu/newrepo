7142aa557c18628e18b21acd952fda09b81bdd03
Aliyu Abubakar <a.aleeyu@yahoo.com>
2023-11-17 10:50:59 +0000

    second update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ it to a variable word_list. We then printed the entire list to the screen.
 We then selected a random fruit from the list, and asked user to input a 
 character. We validated the user input by checking if what the user entered
 was trully a single character, otherwise a user is asked to try again.
+
+
+THI IS AN UPDATE
--- milestone_2.py
+++ milestone_2.py
@@ -4,12 +4,13 @@ fav_fruits = ['apple','orange', 'grapes', 'cheey', 'banana']
 word_list = fav_fruits
 print(word_list)
 
-
+#select a random fruit
 word = random.choice(word_list)
 
-
+#prompt user for an input
 user_input = input('Please eneter a single character: ')
 
+#check if the user input is a single character
 if len(user_input) ==1:
     print('You entered:', user_input)
 else:
