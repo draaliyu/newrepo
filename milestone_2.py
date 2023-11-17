ba279bbcae57e4613431edc99a3215c4dd5db2cd
Aliyu Abubakar <a.aleeyu@yahoo.com>
2023-11-17 11:24:05 +0000

    my first update on hangman

---
+++ milestone_2.py
@@ -0,0 +1,26 @@
+import random
+
+#creating a list of 5 favourite fruits
+favourite_fruits = ['Apple', 'Orange', 'Banana', 'Cheery', 'Mango']
+
+#Assigning the list to a variable, word_list
+word_list= favourite_fruits
+
+#Printing the created list to the screen
+print(word_list)
+
+#Creating a random choice from the list and assign it to variable, word
+word = random.choice(word_list)
+
+#Printing out the choice word
+print(word)
+
+#Prompt the user for an input
+guess = input('Please enter an input: ')
+
+#Check that the user input is a single character
+if len(guess) == 1:
+    print('You entered:', guess)
+else:
+    print('Error: Please enter only one character.')
+
